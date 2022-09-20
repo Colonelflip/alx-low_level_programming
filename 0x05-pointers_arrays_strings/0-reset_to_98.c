a3de7e8c2cc3e52e4771f3dc39894f1b20ff19f7
Colonelflip <adenijiadeoluwa05@gmail.com>
2022-09-19 17:59:55 -0700

    commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,11 @@
-/*
- * File: 0-reset_to_98.c
- * Auth: Brennan D Baraban
- */
-
-#include "holberton.h"
+#include "main.h"
+#include <stdio.h>
 
 /**
- * reset_to_98 - Takes a pointer to an int and updates
- *               the value it points to to 98.
- * @n: The pointer to an int.
+ * reset_to_98 - a function that takes a pointer to an int as parameter
+ * and updates the value it points to to 98
+ * @n: input
+ * Return: n
  */
 void reset_to_98(int *n)
 {
